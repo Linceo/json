3bfe666c5b2dc1ce6aee031baa5f968888b9de5f
Linceo <1574364586@qq.com>
2019-12-04 14:59:05 +0800

    忽略文件

--- node.js
+++ node.js
@@ -1,15 +1,41 @@
-var http = require('http')
 
-http.createServer(function (request, response) {
-
-    // 发送 HTTP 头部 
-    // HTTP 状态值: 200 : OK
-    // 内容类型: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-
-    // 发送响应数据 "Hello World"
-    response.end('Hello World\n');
-}).listen(8888);
-
-// 终端打印如下信息
-console.log('Server running at http://127.0.0.1:8888/');
+/* 引入express框架 */
+const express = require('express');
+const app = express();
+ 
+/* 引入cors */
+const cors = require('cors');
+app.use(cors());
+ 
+/* 引入body-parser */
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+ 
+/* 引入mysql */
+const mysql = require('mysql');
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'test',
+    multipleStatements: true
+})
+conn.connect();
+ 
+/* 监听端口 */
+app.listen(8080, () => {
+    console.log('——————————服务已启动——————————');
+})
+ 
+app.get('/', (req, res) => {
+    res.send('<p style="color:red">服务已启动</p>');
+})
+ 
+app.get('/api/getUserList', (req, res) => {
+    const sqlStr = 'SELECT * FROM users'
+    conn.query(sqlStr, (error, results) => {
+        if (error) return res.json({ code: 10001, message: error})
+        res.json({ code: 10000, message: results})
+    })
+})
